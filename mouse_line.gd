29ff2d1b8f7cd4bbb403eefcecfa090ad5ce5ecd
Zeta80 <axxel1700@gmail.com>
2025-02-26 01:52:57 +0100

    dot signal not finish

--- mouse_line.gd
+++ mouse_line.gd
@@ -4,14 +4,17 @@ extends Node2D
 var pressed := false
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
-	pass # Replace with function body.
-
-
+	var allDot = get_parent().get_node("GridDot").get_children()
+	for dot in allDot:
+		dot.connect("Mouse_enter", Callable(self, "addPointDot"))
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta: float) -> void:
 	updateMouseState()
 	if pressed:
 		line_2d.add_point(get_global_mouse_position())
+
+func addPointDot():
+	pass
 
 func updateMouseState():
 	if Input.is_action_pressed("click"):
